cc53c5566d629648d7fdafd99e974bc2156aee48
Lex Lobov <lexlobov@yandex.ru>
2022-03-29 18:03:06 +0700

    Добавил Enum и класс Orders

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,5 +1,5 @@
 public enum Color {
-    BLACK_PEARL,
-    GRAY_APATHY
+    BLACK,
+    GRAY
 
 }
---
+++ src/main/java/Orders.java
@@ -0,0 +1,38 @@
+public class Orders {
+    private int courierId;
+    private String nearestStation;
+    private int limit;
+    private int page;
+
+    public int getCourierId() {
+        return courierId;
+    }
+
+    public void setCourierId(int courierId) {
+        this.courierId = courierId;
+    }
+
+    public String getNearestStation() {
+        return nearestStation;
+    }
+
+    public void setNearestStation(String nearestStation) {
+        this.nearestStation = nearestStation;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+}
