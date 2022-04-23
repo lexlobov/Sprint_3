9c9128398cabef54570d15e406b60e1f3d9daf4e
Alex Lobov <lexlobov@yandex.ru>
2022-04-23 20:11:53 +0700

    Сделал клиент для логина курьера

--- src/main/java/client/CreateCourierApiClient.java
+++ src/main/java/client/CreateCourierApiClient.java
@@ -1,8 +1,24 @@
 package client;
 
+import Models.ApiAnswers.BadRequestApiAnswer;
+import Models.ApiAnswers.OkApiAnswer;
+import Models.Courier;
+
 public class CreateCourierApiClient extends BaseHTTPClient{
 
     private final String baseUrl = "http://qa-scooter.praktikum-services.ru";
+    private final String createCourierUri = "/api/v1/courier";
 
+    public OkApiAnswer createCourierPositive(String login, String password, String firstName){
+        Courier courier = new Courier(login, password, firstName);
+        OkApiAnswer okApiAnswer = doPostRequest(baseUrl + createCourierUri, courier).as(OkApiAnswer.class);
+        return okApiAnswer;
+    }
+
+    public BadRequestApiAnswer createCourierBadRequest(String login, String password, String firstName){
+        Courier courier = new Courier(login, password, firstName);
+        BadRequestApiAnswer badRequestApiAnswer = doPostRequest(baseUrl + createCourierUri, courier).as(BadRequestApiAnswer.class);
+        return badRequestApiAnswer;
+    }
 
 }
---
+++ src/main/java/client/LoginCourierApiClient.java
@@ -0,0 +1,25 @@
+package client;
+
+
+import Models.ApiAnswers.BadRequestApiAnswer;
+import Models.Courier;
+import Models.CourierLoginOk;
+
+public class LoginCourierApiClient extends BaseHTTPClient {
+
+    private final String baseUrl = "http://qa-scooter.praktikum-services.ru";
+    private final String loginCourierUri = "/api/v1/courier/login";
+
+    public CourierLoginOk createCourierPositive(String login, String password){
+        Courier courier = new Courier(login, password);
+        CourierLoginOk courierLoginOk = doPostRequest(baseUrl + loginCourierUri, courier).as(CourierLoginOk.class);
+        return courierLoginOk;
+    }
+    public BadRequestApiAnswer createCourierBadRequest(String login, String password){
+        Courier courier = new Courier(login, password);
+        BadRequestApiAnswer badRequestApiAnswer = doPostRequest(baseUrl + loginCourierUri, courier).as(BadRequestApiAnswer.class);
+        return badRequestApiAnswer;
+    }
+
+
+}
