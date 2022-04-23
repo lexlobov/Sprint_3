12e2d17936a2a916fb247f920653b919dc6b4eec
Alex Lobov <lexlobov@yandex.ru>
2022-04-23 22:58:07 +0700

    сделал тест на логин курьера

--- src/test/java/CourierLoginTest.java
+++ src/test/java/CourierLoginTest.java
@@ -1,4 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import steps.CourierSteps;
+
 public class CourierLoginTest {
 
+    CourierSteps steps = new CourierSteps();
+    @Before
+    public void setUp(){
+        steps.createCourier();
+    }
 
+    @After
+    public void tearDown(){
+        steps.deleteCourier();
+    }
+
+
+    @Test
+    public void testCourierLogin(){
+        steps.checkCourierLoggedIn();
+    }
 }
--- src/test/java/steps/CourierSteps.java
+++ src/test/java/steps/CourierSteps.java
@@ -1,30 +1,35 @@
 package steps;
 
+import Models.CourierLoginOk;
 import client.CreateCourierApiClient;
 import client.DeleteCourierApiClient;
 import client.LoginCourierApiClient;
 import org.apache.commons.lang3.RandomStringUtils;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class CourierSteps {
 
-    private String login = RandomStringUtils.randomAlphabetic(10);
-    private String password = RandomStringUtils.randomAlphabetic(10);
-    private String firstName = RandomStringUtils.randomAlphabetic(10);
+    private String login;
+    private String password;
+    private String firstName;
     private long courierId;
 
-    public long getCourierId() {
-        return courierId;
-    }
 
-    public void setCourierId(long courierId) {
-        this.courierId = courierId;
-    }
 
     CreateCourierApiClient createCourierApiClient = new CreateCourierApiClient();
     LoginCourierApiClient loginCourierApiClient = new LoginCourierApiClient();
     DeleteCourierApiClient deleteCourierApiClient = new DeleteCourierApiClient();
 
-    public void createCourier (){
+    public void generateCourierData(){
+        setLogin(RandomStringUtils.randomAlphabetic(10));
+        setPassword(RandomStringUtils.randomAlphabetic(10));
+        setFirstName(RandomStringUtils.randomAlphabetic(10));
+    }
+
+    public void createCourier(){
+        generateCourierData();
         createCourierApiClient.createCourierPositive(login, password, firstName);
     }
 
@@ -38,5 +43,42 @@ public class CourierSteps {
 
     }
 
+    public void checkCourierLoggedIn(){
+        CourierLoginOk courierLoginOk = loginCourierApiClient.loginCourierPositive(login, password);
+        assertThat(courierLoginOk.getId()).isNotNull();
+    }
 
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public long getCourierId() {
+        return courierId;
+    }
+
+    public void setCourierId(long courierId) {
+        this.courierId = courierId;
+    }
 }
